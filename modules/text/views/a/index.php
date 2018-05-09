f21d242a90bac2478f607602b3bc78ff76e27f26
alexei.varavin <alexei.varavin@eurostudio.net>
2018-05-09 16:22:43 +0700

    in "Text" module edition link should be on slug instead of text

--- modules/text/views/a/index.php
+++ modules/text/views/a/index.php
@@ -28,9 +28,9 @@ $module = $this->context->module->id;
                 <?php if(IS_ROOT) : ?>
                 <td><?= $item->primaryKey ?></td>
                 <?php endif; ?>
-                <td><a href="<?= Url::to(['/admin/'.$module.'/a/edit', 'id' => $item->primaryKey]) ?>"><?= $item->text ?></a></td>
+                <td><?= $item->text ?></td>
                 <?php if(IS_ROOT) : ?>
-                    <td><?= $item->slug ?></td>
+                    <td><a href="<?= Url::to(['/admin/'.$module.'/a/edit', 'id' => $item->primaryKey]) ?>"><?= $item->slug ?></a></td>
                     <td><a href="<?= Url::to(['/admin/'.$module.'/a/delete', 'id' => $item->primaryKey]) ?>" class="glyphicon glyphicon-remove confirm-delete" title="<?= Yii::t('easyii', 'Delete item') ?>"></a></td>
                 <?php endif; ?>
             </tr>

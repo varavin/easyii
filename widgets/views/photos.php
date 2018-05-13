974a992632e8c56687af741c90a9310d5bb4fc7b
alexei.varavin <alexei.varavin@eurostudio.net>
2018-05-13 15:42:48 +0700

    weird problem with multi-line JS strings, worked fine but suddenly stopped working

--- widgets/views/photos.php
+++ widgets/views/photos.php
@@ -17,26 +17,7 @@ $linkParams = [
     'item_id' => $item_id,
 ];
 
-$photoTemplate = '<tr data-id="{{photo_id}}">'.(IS_ROOT ? '<td>{{photo_id}}</td>' : '').'\
-    <td><a href="{{photo_image}}" class="plugin-box" title="{{photo_description}}" rel="easyii-photos"><img class="photo-thumb" id="photo-{{photo_id}}" src="{{photo_thumb}}"></a></td>\
-    <td>\
-        <textarea class="form-control photo-description">{{photo_description}}</textarea>\
-        <a href="' . Url::to(['/admin/photos/description/{{photo_id}}']) . '" class="btn btn-sm btn-primary disabled save-photo-description">'. Yii::t('easyii', 'Save') .'</a>\
-    </td>\
-    <td>\
-        <input type="checkbox" class="switch" {{photo_show_on_main}} name="" value="{{photo_show_on_main}}" data-id="{{photo_id}}" data-link="/admin/photos" data-link-on="show-on-main-on" data-link-off="show-on-main-off" data-reload="0">
-    </td>\
-    <td class="control vtop">\
-        <div class="btn-group btn-group-sm" role="group">\
-            <a href="' . Url::to(['/admin/photos/up/{{photo_id}}'] + $linkParams) . '" class="btn btn-default move-up" title="'. Yii::t('easyii', 'Move up') .'"><span class="glyphicon glyphicon-arrow-up"></span></a>\
-            <a href="' . Url::to(['/admin/photos/down/{{photo_id}}'] + $linkParams) . '" class="btn btn-default move-down" title="'. Yii::t('easyii', 'Move down') .'"><span class="glyphicon glyphicon-arrow-down"></span></a>\
-            <a href="' . Url::to(['/admin/photos/image/{{photo_id}}'] + $linkParams) . '" class="btn btn-default change-image-button" title="'. Yii::t('easyii', 'Change image') .'"><span class="glyphicon glyphicon-floppy-disk"></span></a>\
-            <a href="' . Url::to(['/admin/photos/delete/{{photo_id}}']) . '" class="btn btn-default color-red delete-photo" title="'. Yii::t('easyii', 'Delete item') .'"><span class="glyphicon glyphicon-remove"></span></a>\
-            <input type="file" name="Photo[image]" class="change-image-input hidden">\
-        </div>\
-    </td>\
-</tr>';
-$this->registerJs("
+$photoTemplate = '<tr data-id="{{photo_id}}">'.(IS_ROOT ? '<td>{{photo_id}}</td>' : '').'    <td><a href="{{photo_image}}" class="plugin-box" title="{{photo_description}}" rel="easyii-photos"><img class="photo-thumb" id="photo-{{photo_id}}" src="{{photo_thumb}}"></a></td>    <td>        <textarea class="form-control photo-description">{{photo_description}}</textarea>        <a href="' . Url::to(['/admin/photos/description/{{photo_id}}']) . '" class="btn btn-sm btn-primary disabled save-photo-description">'. Yii::t('easyii', 'Save') .'</a>    </td>    <td>        <input type="checkbox" class="switch" {{photo_show_on_main}} name="" value="{{photo_show_on_main}}" data-id="{{photo_id}}" data-link="/admin/photos" data-link-on="show-on-main-on" data-link-off="show-on-main-off" data-reload="0">    </td>    <td class="control vtop">        <div class="btn-group btn-group-sm" role="group">            <a href="' . Url::to(['/admin/photos/up/{{photo_id}}'] + $linkParams) . '" class="btn btn-default move-up" title="'. Yii::t('easyii', 'Move up') .'"><span class="glyphicon glyphicon-arrow-up"></span></a>            <a href="' . Url::to(['/admin/photos/down/{{photo_id}}'] + $linkParams) . '" class="btn btn-default move-down" title="'. Yii::t('easyii', 'Move down') .'"><span class="glyphicon glyphicon-arrow-down"></span></a>            <a href="' . Url::to(['/admin/photos/image/{{photo_id}}'] + $linkParams) . '" class="btn btn-default change-image-button" title="'. Yii::t('easyii', 'Change image') .'"><span class="glyphicon glyphicon-floppy-disk"></span></a>            <a href="' . Url::to(['/admin/photos/delete/{{photo_id}}']) . '" class="btn btn-default color-red delete-photo" title="'. Yii::t('easyii', 'Delete item') .'"><span class="glyphicon glyphicon-remove"></span></a>            <input type="file" name="Photo[image]" class="change-image-input hidden">        </div>    </td></tr>';$this->registerJs("
 var photoTemplate = '{$photoTemplate}';
 ", \yii\web\View::POS_HEAD);
 $photoTemplate = str_replace('>\\', '>', $photoTemplate);

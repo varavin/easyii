46ce55d8a10531e16655213d4b6ef0a3da19de44
alexei.varavin <alexei.varavin@eurostudio.net>
2018-05-17 00:10:51 +0700

    Миграция для модуля блога.
    Команда:
    
    $ php yii migrate --migrationPath=@app/../vendor/noumo/easyii/migrations --migrationTable="easyii_migration"

---
+++ migrations/m180516_235512_add_blog_module.php
@@ -0,0 +1,41 @@
+<?php
+
+use yii\db\Schema;
+use yii\db\Migration;
+
+class m180510_154020_add_show_on_main_properties_to_photos extends Migration
+{
+    public function up()
+    {
+		$this->createTable(Blog::tableName(), [
+			'blog_id' => 'pk',
+			'title' => Schema::TYPE_STRING . '(128) NOT NULL',
+			'image' => Schema::TYPE_STRING . '(128) DEFAULT NULL',
+			'short' => Schema::TYPE_STRING . '(1024) DEFAULT NULL',
+			'text' => Schema::TYPE_TEXT . ' NOT NULL',
+			'slug' => Schema::TYPE_STRING . '(128) DEFAULT NULL',
+			'time' => Schema::TYPE_INTEGER .  " DEFAULT '0'",
+			'views' => Schema::TYPE_INTEGER . " DEFAULT '0'",
+			'status' => Schema::TYPE_BOOLEAN . " DEFAULT '1'"
+		], $this->engine);
+		$this->createIndex('slug', News::tableName(), 'slug', true);
+    }
+
+    public function down()
+    {
+        echo "m180510_154020_add_show_on_main_properties_to_photos cannot be reverted.\n";
+
+        return false;
+    }
+    
+    /*
+    // Use safeUp/safeDown to run migration code within a transaction
+    public function safeUp()
+    {
+    }
+    
+    public function safeDown()
+    {
+    }
+    */
+}
